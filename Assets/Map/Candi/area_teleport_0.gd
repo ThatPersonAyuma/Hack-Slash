dbaa06a66a334826496212c20c5f45020189e787
HiddenCanvas <mhabibarrasyid03@gmail.com>
2025-08-24 12:42:40 +0700

    ngambil

---
+++ Assets/Map/Candi/area_teleport_0.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var entered = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
